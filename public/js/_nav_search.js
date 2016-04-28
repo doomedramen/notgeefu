6ae56cf036c922aacbdf82fc2c261bb9a03eee77
Martin Page <wookoouk@gmail.com>
2016-04-28 15:20:20 +0100

    custom port

--- public/js/_nav_search.js
+++ public/js/_nav_search.js
@@ -1,9 +1,31 @@
 $(function () {
-  $('#nav-search').find('span').on('click', function () {
-    //var current = $(this).attr('data-icon');
-    //console.log('current code is', current);
-    var spinner = '';
-    $(this).attr('data-icon', spinner);
-    $(this).addClass('spinner');
+
+  var navSearch = $('#nav-search');
+
+  var navSearchSpinner = navSearch.find('span');
+  var navSearchInput = navSearch.find('input');
+
+  navSearch.on('click', function () {
+    $(this).find('input').focus();
   });
+
+  navSearchInput.on('change keyup paste', function () {
+    var current = 'U';
+    var spinner = '';
+
+    //start spinning
+    navSearchSpinner.attr('data-icon', spinner);
+    navSearchSpinner.addClass('spinner');
+
+    setTimeout(function () {
+      navSearchSpinner.attr('data-icon', current);
+      navSearchSpinner.removeClass('spinner');
+    }, 1000);
+
+    //after result
+    //$(this).attr('data-icon', current);
+    //$(this).removeClass('spinner');
+
+  });
+
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./app');
 var log = require('./lib/log');
 
-var port = process.env.PORT || '3000';
+var port = process.env.NGFPORT || '3000';
 
 app.listen(port, '0.0.0.0', function () {
   log.success('Listening on port', port);
